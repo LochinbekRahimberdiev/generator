1872845aa8d67811114b113db09029a5d59932b9
Lochinbek <rahimberdiyevlochinbek43@gmail.com>
2024-06-13 20:53:09 +0300

    generator.py

--- generator.py
+++ generator.py
@@ -1,65 +1,43 @@
-# range(x) => (0, x -1) step = 1
-# range(x, y) => (x, y-1) step = 1
-# range(x, y, z) => (x, y - 1) step = z
 import time
 
 
-class Range:
+class CloneRange:
     def __init__(self, start, stop=None, step=1):
         if stop is None:
-            self.stop = start   # stop = 10
-            self.start = -step  # start = -1
-            self.step = step    # step = 1
+            self.start = 0
+            self.stop = start
+            self.step = step
         else:
-            self.start = start - step
+            self.start = start
             self.stop = stop
             self.step = step
 
     def __iter__(self):
-        # print("call iter")
         return self
 
     def __next__(self):
-        # if self.step > 0:
-        # print("next iter")
-        if self.start > self.stop and self.step > 0:
+        if self.start >= self.stop and self.step > 0:
             raise StopIteration
-
-        elif self.start > self.stop and self.step < 0:
+        elif self.start <= self.stop and self.step < 0:
+            raise StopIteration
+        else:
+            current = self.start
             self.start += self.step
-            if self.stop >= self.start:
-                raise StopIteration
-            return self.start
-
-        elif self.start < self.stop and self.step > 0:
-            self.start += self.step
-            if self.start >= self.stop:
-                raise StopIteration
-            return self.start
+            return current
 
 
-
-for i in Range(2, 25, 2.5):
+# Test with float step
+for i in CloneRange(2, 25, 2.5):
     print(i)
 
+# Test with integer step
+for i in CloneRange(2, 10, 2):
+    print(i)
 
+# Test with negative step
+for i in CloneRange(10, 2, -2):
+    print(i)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Test with single argument
+for i in CloneRange(10):
+    print(i)
